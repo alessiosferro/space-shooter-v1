c52339666ed374264f451ec3be7f6fe5bcfcc9cc
Alessio Sferro <sferro.alessio@gmail.com>
2024-04-08 15:54:41 +0200

    feat: add bullets, background

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 import {Flex, VisuallyHidden} from "@chakra-ui/react";
 import {useEffect, useRef} from "react";
 
-import ships from './assets/SpaceShooterAssetPack_Ships.png';
+import ships from './assets/ships.png';
+import projectiles from './assets/projectiles.png';
+import backgrounds from './assets/backgrounds.png';
 
 const CANVAS_WIDTH = 350;
 const CANVAS_HEIGHT = 600;
@@ -12,11 +14,15 @@ const PLAYER_SIZE = PLAYER_SIZE_MULTIPLIER * 8;
 
 function App() {
     const canvas = useRef<HTMLCanvasElement | null>(null);
-    const shipsImage = useRef<HTMLImageElement | null>(null);
+    const shipsImageRef = useRef<HTMLImageElement | null>(null);
+    const projectilesImageRef = useRef<HTMLImageElement | null>(null);
+    const animationFrameId = useRef<number | null>(null);
+    const backgroundsImageRef = useRef<HTMLImageElement | null>(null)
+    const fireBulletTimeoutId = useRef<number | null>(null);
 
     const player = useRef({
-        velocityX: 1,
-        velocityY: .8,
+        velocityX: 2,
+        velocityY: 1.5,
         speedX: 0,
         speedY: 0,
         hp: 5,
@@ -26,121 +32,177 @@ function App() {
         keysPressed: new Set<string>([])
     });
 
-    function updatePlayerStatus(status: 'idle' | 'moving-left' | 'moving-right') {
-        player.current = {
-            ...player.current,
-            status
-        }
-    }
+    const game = useRef({
+        backgroundY: 0,
+    })
 
-    function updateSpeedX(speedX: number) {
-        player.current = {
-            ...player.current,
-            speedX
-        }
-    }
+    const bullets = useRef<Bullet[]>([]);
 
-    function updateSpeedY(speedY: number) {
-        player.current = {
-            ...player.current,
-            speedY
-        }
-    }
-
-    function updatePlayerPosition() {
-        const {x, speedX, y, speedY, status} = player.current;
-
-        if (status === 'moving-left' && x - speedX < -PLAYER_SIZE) {
-            return void (player.current = {
-                ...player.current,
-                y: y + speedY,
-                x: CANVAS_WIDTH + PLAYER_SIZE
-            })
-        }
-
-        if (status === 'moving-right' && x + speedX > CANVAS_WIDTH + PLAYER_SIZE) {
-            return void (player.current = {
-                ...player.current,
-                y: y + speedY,
-                x: -PLAYER_SIZE
-            })
-        }
-
-        player.current = {
-            ...player.current,
-            x: x + speedX,
-            y: y + speedY
-        }
-    }
-
-    function handleKeyUp(e: KeyboardEvent) {
-        updateKeysPressed(Array.from(player.current.keysPressed).filter(key => key !== e.code));
-
-        const isMovingVertically = ['ArrowUp', 'ArrowDown'].find(key => player.current.keysPressed.has(key));
-        const isMovingHorizontally = ['ArrowLeft', 'ArrowRight'].find(key => player.current.keysPressed.has(key));
-
-        if (!isMovingVertically) {
-            updateSpeedY(0);
-        }
-
-        if (!isMovingHorizontally) {
-            updateSpeedX(0);
-        }
-
-        if (!isMovingHorizontally && !isMovingVertically) {
-            updatePlayerStatus('idle');
-        }
-    }
-
-    function updateKeysPressed(keysPressed: string[]) {
-        player.current = {
-            ...player.current,
-            keysPressed: new Set(keysPressed)
-        }
-    }
-
-    function handleKeyDown(e: KeyboardEvent) {
-        const {
-            velocityX,
-            velocityY,
-            keysPressed
-        } = player.current;
-
-        updateKeysPressed([
-            ...Array.from(keysPressed),
-            e.code
-        ]);
-
-        switch (e.code) {
-            case "Space":
-                break;
-
-            case "ArrowDown":
-                updateSpeedY(velocityY);
-                break;
-
-            case "ArrowLeft":
-                updatePlayerStatus('moving-left');
-                updateSpeedX(-velocityX);
-                break;
-
-            case "ArrowRight":
-                updatePlayerStatus('moving-right');
-                updateSpeedX(velocityX);
-                break;
-
-            case "ArrowUp":
-                updateSpeedY(-velocityY);
-                break;
-        }
-    }
-
-    const game = () => {
+    const main = () => {
         if (!canvas.current) return;
 
         const ctx = canvas.current.getContext('2d') as CanvasRenderingContext2D;
 
+        addEventListener('keydown', handleKeyDown);
+        addEventListener('keyup', handleKeyUp);
+
         ctx.imageSmoothingEnabled = false;
+
+        function updatePlayerStatus(status: 'idle' | 'moving-left' | 'moving-right') {
+            player.current = {
+                ...player.current,
+                status
+            }
+        }
+
+        function updateSpeedX(speedX: number) {
+            player.current = {
+                ...player.current,
+                speedX
+            }
+        }
+
+        function updateSpeedY(speedY: number) {
+            player.current = {
+                ...player.current,
+                speedY
+            }
+        }
+
+        function updatePlayerPosition() {
+            const {x, speedX, y, speedY} = player.current;
+
+            const nextY = y + speedY;
+            const nextX = x + speedX;
+
+            const isYOverflow = nextY > CANVAS_HEIGHT - PLAYER_SIZE || nextY < PLAYER_SIZE;
+            const isXOverflow = nextX > CANVAS_WIDTH - PLAYER_SIZE || nextX < 0;
+
+            player.current = {
+                ...player.current,
+                ...(!isYOverflow && {y: nextY}),
+                ...(!isXOverflow && {x: nextX})
+            }
+        }
+
+        function handleKeyUp(e: KeyboardEvent) {
+            updateKeysPressed(Array.from(player.current.keysPressed).filter(key => key !== e.code));
+
+            const isMovingVertically = ['ArrowUp', 'ArrowDown'].find(key => player.current.keysPressed.has(key));
+            const isMovingHorizontally = ['ArrowLeft', 'ArrowRight'].find(key => player.current.keysPressed.has(key));
+
+            if (!isMovingVertically) {
+                updateSpeedY(0);
+            }
+
+            if (!isMovingHorizontally) {
+                updateSpeedX(0);
+            }
+
+            if (!isMovingHorizontally && !isMovingVertically) {
+                updatePlayerStatus('idle');
+            }
+        }
+
+        function updateKeysPressed(keysPressed: string[]) {
+            player.current = {
+                ...player.current,
+                keysPressed: new Set(keysPressed)
+            }
+        }
+
+        function fireBullet() {
+            const {x, y} = player.current;
+
+            bullets.current = [
+                ...bullets.current,
+                {
+                    x: x + 20,
+                    y,
+                    velocityX: 0,
+                    velocityY: -5,
+                    sx: 4,
+                    sy: 4,
+                    sw: 1,
+                    sh: 1,
+                }
+            ];
+        }
+
+        function updateBulletsPositions() {
+            bullets.current = bullets.current
+                .filter(bullet => bullet.y >= 0)
+                .map(bullet => ({
+                    ...bullet,
+                    x: bullet.x + bullet.velocityX,
+                    y: bullet.y + bullet.velocityY
+                }));
+        }
+
+        function drawBullets() {
+            for (const bullet of bullets.current) {
+                ctx.drawImage(projectilesImageRef.current!, bullet.sx, bullet.sy, bullet.sw, bullet.sh, bullet.x, bullet.y, 8, 8);
+            }
+        }
+
+        function handleKeyDown(e: KeyboardEvent) {
+            const {
+                velocityX,
+                velocityY,
+                keysPressed
+            } = player.current;
+
+            if (fireBulletTimeoutId.current) {
+                clearTimeout(fireBulletTimeoutId.current);
+            }
+
+            updateKeysPressed([
+                ...Array.from(keysPressed),
+                e.code
+            ]);
+
+            switch (e.code) {
+                case "Space": {
+                    fireBulletTimeoutId.current = setTimeout(() => {
+                        fireBullet();
+                    }, 100)
+
+                    break;
+                }
+
+                case "ArrowDown":
+                    updateSpeedY(velocityY);
+                    break;
+
+                case "ArrowLeft":
+                    updatePlayerStatus('moving-left');
+                    updateSpeedX(-velocityX);
+                    break;
+
+                case "ArrowRight":
+                    updatePlayerStatus('moving-right');
+                    updateSpeedX(velocityX);
+                    break;
+
+                case "ArrowUp":
+                    updateSpeedY(-velocityY);
+                    break;
+            }
+        }
+
+        function updateBackgroundPosition() {
+            game.current = {
+                ...game.current,
+                backgroundY: (game.current.backgroundY % CANVAS_HEIGHT) + .2
+            }
+        }
+
+        function drawBackground() {
+            const {backgroundY} = game.current;
+
+            ctx.drawImage(backgroundsImageRef.current!, 0, 0, 127, 255, 0, backgroundY - CANVAS_HEIGHT, 350, 600);
+            ctx.drawImage(backgroundsImageRef.current!, 0, 0, 127, 255, 0, backgroundY, 350, 600);
+        }
 
         function drawPlayer() {
             const {
@@ -153,21 +215,21 @@ function App() {
                 case 'idle': {
                     const sizeW = 8;
                     const sizeH = 8;
-                    ctx.drawImage(shipsImage.current!, 8, 0, 8, 8, x, y, sizeW * PLAYER_SIZE_MULTIPLIER, sizeH * PLAYER_SIZE_MULTIPLIER)
+                    ctx.drawImage(shipsImageRef.current!, 8, 0, 8, 8, x, y, sizeW * PLAYER_SIZE_MULTIPLIER, sizeH * PLAYER_SIZE_MULTIPLIER)
                     break;
                 }
 
                 case 'moving-left': {
                     const sizeW = 6;
                     const sizeH = 8;
-                    ctx.drawImage(shipsImage.current!, 0, 0, 6, 8, x, y, sizeW * PLAYER_SIZE_MULTIPLIER, sizeH * PLAYER_SIZE_MULTIPLIER)
+                    ctx.drawImage(shipsImageRef.current!, 0, 0, 6, 8, x, y, sizeW * PLAYER_SIZE_MULTIPLIER, sizeH * PLAYER_SIZE_MULTIPLIER)
                     break;
                 }
 
                 case 'moving-right': {
                     const sizeW = 6;
                     const sizeH = 8;
-                    ctx.drawImage(shipsImage.current!, 18, 0, 6, 8, x, y, sizeW * PLAYER_SIZE_MULTIPLIER, sizeH * PLAYER_SIZE_MULTIPLIER)
+                    ctx.drawImage(shipsImageRef.current!, 18, 0, 6, 8, x, y, sizeW * PLAYER_SIZE_MULTIPLIER, sizeH * PLAYER_SIZE_MULTIPLIER)
                     break;
                 }
             }
@@ -177,24 +239,31 @@ function App() {
         function loop() {
             ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
+            updateBackgroundPosition();
             updatePlayerPosition();
-            drawPlayer();
+            updateBulletsPositions();
 
-            requestAnimationFrame(loop);
+            drawBackground();
+            drawPlayer();
+            drawBullets();
+
+            animationFrameId.current = requestAnimationFrame(loop);
         }
 
         loop();
-    }
-
-    useEffect(() => {
-        addEventListener('keydown', handleKeyDown);
-        addEventListener('keyup', handleKeyUp);
-
-        game();
 
         return () => {
             removeEventListener('keydown', handleKeyDown);
             removeEventListener('keyup', handleKeyUp);
+            cancelAnimationFrame(animationFrameId.current!);
+        }
+    }
+
+    useEffect(() => {
+        const clean = main();
+
+        return () => {
+            clean?.();
         }
     }, []);
 
@@ -212,7 +281,11 @@ function App() {
                     width={CANVAS_WIDTH}/>
 
             <VisuallyHidden>
-                <img ref={shipsImage} style={{transform: "scale(10)"}} src={ships} alt=""/>
+                <img ref={shipsImageRef} src={ships} alt=""/>
+
+                <img ref={projectilesImageRef} src={projectiles} alt=""/>
+
+                <img ref={backgroundsImageRef} src={backgrounds} alt=""/>
 
                 <footer>
                     <p>Sviluppato da Alessio Sferro</p>
@@ -220,6 +293,20 @@ function App() {
             </VisuallyHidden>
         </Flex>
     )
+}
+
+type Bullet = {
+    x: number;
+    y: number;
+
+    // coordinates in image
+    sx: number;
+    sy: number;
+    sw: number;
+    sh: number;
+
+    velocityX: number;
+    velocityY: number;
 }
 
 export default App
